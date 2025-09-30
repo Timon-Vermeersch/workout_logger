907c5fff9d51b375fa86b47ebf64baea31fa5a6d
Timon-Vermeersch <timon.vermeersch@hotmail.com>
2025-09-30 17:54:03 +0200

    Removed_Drizzle

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,11 +1,10 @@
-// See https://svelte.dev/docs/kit/types#app.d.ts
-// for information about these interfaces
+// See https://svelte.dev/docs/kit/types#app.d.ts for information about these interfaces
 declare global {
 	namespace App {
-		interface Locals {
-			user: import('$lib/server/auth').SessionValidationResult['user'];
-			session: import('$lib/server/auth').SessionValidationResult['session'];
-		}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Error {}
+		// interface Platform {}
 	}
 }
 
