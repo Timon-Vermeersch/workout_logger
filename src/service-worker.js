d4e17f77b63cf912ee836986d032e589061462f1
YOLOTANKERSWAG2023 <timon.vermeersch@hotmail.com>
2026-03-08 02:03:48 +0100

    PWA-Setup

---
+++ src/service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "app-v1";
+
+const APP_ASSETS = [
+  "/",
+  "/manifest.webmanifest",
+  "/favicon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(APP_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
